821aae0028440a75f940560ee1aa9ec88ed3d7d9
Michael Fenwick <mickyfen17@aol.com>
2017-03-13 14:47:45 -0600

    build out DistrictRespoitory class

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.reduceData(data);
+  }
 
+  reduceData(data) {
+    return data.reduce((obj, val) => {
+      if (!obj[val.Location]) {
+        obj[val.Location] = []
+      }
+      obj[val.Location].push(val)
+      return obj
+    }, {})
+  }
 }
