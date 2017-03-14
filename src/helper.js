c481bca771d3a4a82c4d99df76bfabc1e272e5da
Michael Fenwick <mickyfen17@aol.com>
2017-03-13 21:34:29 -0600

    alter reduce function to hold combinedData array

--- src/helper.js
+++ src/helper.js
@@ -5,11 +5,47 @@ export default class DistrictRepository {
 
   reduceData(data) {
     return data.reduce((obj, val) => {
-      if (!obj[val.Location]) {
-        obj[val.Location] = []
+      const { Location, TimeFrame, Data } = val;
+      if (!obj[Location]) {
+        obj[Location] = []
+        obj[Location].CombinedData = []
       }
-      obj[val.Location].push(val)
+      obj[Location].push(val)
+      obj[Location].CombinedData.push({ [TimeFrame] : Data })
       return obj
     }, {})
   }
-}
+  // console.log(obj['Colorado'].sort((a, b) => a.TimeFrame - b.TimeFrame));
+
+  // findByName(location) {
+  //   const reduced = Object.keys(this.data).reduce((arr, val) => {
+  //     val === location && (arr.push(...this.data[val]));
+  //     return arr
+  //   }, [])
+  //   .reduce((acc, obj, i) => {
+  //     const { Location, TimeFrame, DataFormat, Data } = obj;
+  //     console.log(TimeFrame);
+  //     !acc.location && (acc.location = Location)
+  //     let test = { [TimeFrame] : Data }
+      // console.log(test)
+      // acc.data = {};
+      // console.log(test);
+      // acc.data = test
+      // if(test[TimeFrame]) {
+      //   console.log(i);
+      //   test[TimeFrame] = Data
+      // }
+      // if(acc.data) {
+      //   acc.data[TimeFrame] = Data
+      // }
+      // acc.data {
+      //   [TimeFrame] : Data
+      // }
+      // acc.data = {[TimeFrame] : Data}
+      // console.log(acc.data)
+    //   return acc
+    // }, {})
+    // console.log(reduced.data);
+    // return reduced
+  // }
+};
