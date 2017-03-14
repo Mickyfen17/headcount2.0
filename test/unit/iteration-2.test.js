12e9b82ab05a1b9297e54014b48ccb35cdb2aec6
Michael Fenwick <mickyfen17@aol.com>
2017-03-14 15:01:32 -0600

    skip tests

--- test/unit/iteration-2.test.js
+++ test/unit/iteration-2.test.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import DistrictRepository from '../../src/helper.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
-import SchoolCard from '../../src/components/SchoolCard'
 
-describe('Visual Layer iteration 2', () => {
+describe.skip('Visual Layer iteration 2', () => {
   const district = new DistrictRepository(kinderData);
 
-  it.only('testing SchoolCard', () => {
+  it('testing SchoolCard', () => {
     const wrapper = shallow(<SchoolCard />)
     console.log(wrapper.debug());
   })
-
-
 })
