ba70c072bbf6911e47f73be1ce9c8de1d42c2e82
Michael Fenwick <mickyfen17@aol.com>
2017-03-14 15:00:18 -0600

    App grabing data, setting State and passing state to CardList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import SchoolCard from './components/SchoolCard'
+import CardList from './components/CardList'
 import './App.css';
+import DistrictRepository from './helper.js';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      countyStats: {}
+    }
+  }
+
+  componentDidMount() {
+    const district = new DistrictRepository(kinderData);
+    this.setState ({
+       countyStats: district.data,
+     })
+  }
+  
   render() {
     return (
       <div>
         Welcome To Headcount 2.0
-        <SchoolCard />
+        <CardList { ...this.state }/>
       </div>
     );
   }
