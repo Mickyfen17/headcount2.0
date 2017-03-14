1e1da6da8562b0cbb149c6dc5339a224943dffbe
Michael Fenwick <mickyfen17@aol.com>
2017-03-13 23:58:33 -0600

    built constructor to pass iteration1 part 1

--- src/helper.js
+++ src/helper.js
@@ -5,47 +5,33 @@ export default class DistrictRepository {
 
   reduceData(data) {
     return data.reduce((obj, val) => {
-      const { Location, TimeFrame, Data } = val;
+      let { Location, TimeFrame, Data } = val;
       if (!obj[Location]) {
+        Location = Location.toUpperCase()
         obj[Location] = []
         obj[Location].CombinedData = []
       }
       obj[Location].push(val)
-      obj[Location].CombinedData.push({ [TimeFrame] : Data })
+      obj[Location].CombinedData.push([TimeFrame, Data])
       return obj
     }, {})
   }
-  // console.log(obj['Colorado'].sort((a, b) => a.TimeFrame - b.TimeFrame));
 
-  // findByName(location) {
-  //   const reduced = Object.keys(this.data).reduce((arr, val) => {
-  //     val === location && (arr.push(...this.data[val]));
-  //     return arr
-  //   }, [])
-  //   .reduce((acc, obj, i) => {
-  //     const { Location, TimeFrame, DataFormat, Data } = obj;
-  //     console.log(TimeFrame);
-  //     !acc.location && (acc.location = Location)
-  //     let test = { [TimeFrame] : Data }
-      // console.log(test)
-      // acc.data = {};
-      // console.log(test);
-      // acc.data = test
-      // if(test[TimeFrame]) {
-      //   console.log(i);
-      //   test[TimeFrame] = Data
-      // }
-      // if(acc.data) {
-      //   acc.data[TimeFrame] = Data
-      // }
-      // acc.data {
-      //   [TimeFrame] : Data
-      // }
-      // acc.data = {[TimeFrame] : Data}
-      // console.log(acc.data)
-    //   return acc
-    // }, {})
-    // console.log(reduced.data);
-    // return reduced
-  // }
+  findByName(location) {
+    if(location === undefined || !this.data[location.toUpperCase()]) {
+      return undefined;
+    }
+    location = location.toUpperCase()
+    const { CombinedData } = this.data[location]
+    const currLocation = this.data[location][0].Location
+
+    return CombinedData.sort((a, b) => a[0] - b[0])
+    .reduce((arr, val) => {
+      const [ year, data ] = val
+      !arr.location && (arr.location = currLocation)
+      arr.data[year] = data === 'N/A' ? 0 : Math.round(1000*data)/1000;
+      return arr
+    }, { data : {} })
+  }
+
 };
