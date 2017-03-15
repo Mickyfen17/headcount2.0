08024157e7019b93387a28dc3f9387509115ec47
Michael Fenwick <mickyfen17@aol.com>
2017-03-15 14:58:02 -0600

    add state to hold search input data

--- src/container/App.js
+++ src/container/App.js
@@ -4,25 +4,48 @@ import './App.css';
 import DistrictRepository from '../helper.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
+const district = new DistrictRepository(kinderData);
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      countyStats: {}
+      countyStats: {},
+      searchText: ''
     }
   }
 
   componentDidMount() {
-    const district = new DistrictRepository(kinderData);
     this.setState ({
        countyStats: district.data,
      })
+  }
+
+  handleSearch(e) {
+    this.setState({
+      searchText: e.target.value
+    })
+    this.searchCouties()
+  }
+
+  searchCouties() {
+    const filteredCountiesObject = district.findAllMatches(this.state.searchText)
+    this.setState({
+      countyStats: filteredCountiesObject
+    })
   }
 
   render() {
     return (
       <section className='main-section'>
         <h1>Welcome To Headcount 2.0</h1>
+        <input
+          className='search-input'
+          type='text'
+          value={ this.state.searchText }
+          placeholder='Search School Counties'
+          onChange={ e => this.handleSearch(e) }
+        />
         <CardList { ...this.state }/>
       </section>
     );
