ffa9022d2ff7771a8085018aec6d8f0b46694dfb
Michael Fenwick <mickyfen17@aol.com>
2017-03-16 20:54:01 -0600

    add first tests for CardList

--- src/components/CardList/CardList.test.js
+++ src/components/CardList/CardList.test.js
@@ -4,21 +4,46 @@ import { shallow } from 'enzyme';
 import DistrictRepository from '../../helper.js';
 import kinderData from '../../../data/kindergartners_in_full_day_program.js';
 
-describe.skip('testing the App component', () => {
+describe('testing the App component', () => {
   const district = new DistrictRepository(kinderData);
+  const data = {
+    data: {
+      2004: 0.24,
+      2005: 0.278,
+      2006: 0.337,
+      2007: 0.395,
+      2008: 0.536,
+      2009: 0.598,
+      2010: 0.64,
+      2011: 0.672,
+      2012: 0.695,
+      2013: 0.703,
+      2014: 0.741
+    },
+    location: "COLORADO"
+  }
 
-  it('should have props for four types of state', () => {
+  it('should have a cards wrapper to hold all card rendered to the DOM', () => {
     const wrapper = shallow(
-      <CardList countyStats={ { test: 'district'} }/>
+      <CardList
+        countyStats={ district }
+        selectedCards={ [0, 1] }
+        handleClick={''}
+      />
     )
-    // console.log(district);
-    // const state = wrapper.state()
-    // console.log(state);
+
+    expect(wrapper.find('.cards-wrapper')).toHaveLength(1)
+  })
+
+  it.skip('should have props for four types of state', () => {
+    const wrapper = shallow(
+      <CardList
+        countyStats={ district }
+        selectedCards={ [0, 1] }
+        handleClick={''}
+      />
+    )
     console.log(wrapper.debug());
-    // expect(state.countyStats).toEqual({});
-    // expect(state.searchText).toEqual('');
-    // expect(state.selectedCards).toEqual([]);
-    // expect(state.toCompare).toEqual([]);
   })
 
 })
