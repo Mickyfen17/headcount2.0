214134b5378e2388e8efcc7fbd1b527b17cd3cf4
Michael Fenwick <mickyfen17@aol.com>
2017-03-17 08:30:40 -0600

    add test to check props content

--- src/components/CardsToCompare/CardsToCompare.test.js
+++ src/components/CardsToCompare/CardsToCompare.test.js
@@ -69,8 +69,30 @@ describe('testing the CardsToCompare component', () => {
         handleClick={ () => {} }
       />
     )
-
+    console.log(wrapper.debug());
     expect(wrapper.find(SchoolCountyCard)).toHaveLength(2)
+  })
+
+  it('should hold the contect to be rendered to the DOM within props', () => {
+    const wrapper = shallow(
+      <CardsToCompare
+        cardsToCompare={ data }
+        handleClick={ () => {} }
+      />
+    )
+    const countyStats = {'2004': 0.24,'2005': 0.278,'2006': 0.337,'2007': 0.395,
+      '2008': 0.536,'2009': 0.598,'2010': 0.64,'2011': 0.672,'2012': 0.695,
+      '2013': 0.703,'2014': 0.741 }
+
+    expect(wrapper.find(SchoolCountyCard).first()
+                                         .props().location)
+                                         .toEqual('COLORADO')
+    expect(wrapper.find(SchoolCountyCard).first()
+                                         .props().countyStats)
+                                         .toEqual(countyStats)
+    expect(wrapper.find(SchoolCountyCard).first()
+                                         .props().index)
+                                         .toEqual(1489720931335)
   })
 
 })
